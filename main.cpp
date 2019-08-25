0e99b8166626e145a066e7565cda5a0b73383ba2
nxyexiong <nxyexiong@gmail.com>
2019-08-25 19:38:00 +0800

    makefile

--- main.cpp
+++ main.cpp
@@ -1,12 +1,5 @@
-#include <lwip/init.h>
-#include <lwip/ip_addr.h>
-#include <lwip/priv/tcp_priv.h>
-#include <lwip/netif.h>
-#include <lwip/tcp.h>
-#include <lwip/ip4_frag.h>
-#include <lwip/nd6.h>
-#include <lwip/ip6_frag.h>
+#include <stdio.h>
 
 int main() {
-    return 1;
+    printf("helloworld");
 }
