58fade315f6e0c6a1f0fe0dbb05eda6938cb92d5
nxyexiong <nxyexiong@gmail.com>
2019-08-29 23:57:52 +0800

    complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import os
+import sys
 
 from udp_server import UDPServer
 from tcp_server import TCPServer
@@ -8,14 +9,35 @@ from utils import open_write_pipe, open_read_pipe
 TUN2SOCKS_READ_FIFO = "/tmp/tun2socks_read"
 TUN2SOCKS_WRITE_FIFO = "/tmp/tun2socks_write"
 
+enable_udp = False
+enable_tcp = False
+
+
+def parse_args(args):
+    global enable_udp
+    global enable_tcp
+
+    count = len(args)
+    for i in range(count):
+        if args[i] == '--udp':
+            enable_udp = True
+        elif args[i] == '--tcp':
+            enable_tcp = True
+
+
 if __name__ == "__main__":
+    parse_args(sys.argv)
+
     write_fd = open_write_pipe(TUN2SOCKS_READ_FIFO)
     read_fd = open_read_pipe(TUN2SOCKS_WRITE_FIFO)
 
-    #udpserver = UDPServer(6666, write_fd, read_fd)
-    #udpserver.run()
-    tcpserver = TCPServer(7777, write_fd, read_fd)
-    tcpserver.run()
+    if enable_udp:
+        udpserver = UDPServer(6666, write_fd, read_fd)
+        udpserver.run()
+
+    if enable_tcp:
+        tcpserver = TCPServer(7777, write_fd, read_fd)
+        tcpserver.run()
 
     while True:
         time.sleep(1)
