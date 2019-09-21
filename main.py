f90358a9c464dd84a67dcd2088e2ca6f5e89fedd
nxyexiong <nxyexiong@gmail.com>
2019-09-21 18:53:33 +0800

    startup params

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import sys
 
 from tuntap_utils import init_tun, uninit_tun
 from controller import Controller
@@ -7,6 +8,21 @@ from logger import LOGGER
 
 
 if __name__ == "__main__":
+    # check params
+    args = sys.argv
+    port = None
+    secret = None
+    if len(args < 3):
+        LOGGER.error("wrong parameter count")
+        sys.exit(1)
+    try:
+        port = int(args[1])
+        secret = str(args[2]).encode('utf-8')
+    except Exception:
+        LOGGER.error("wrong parameter type")
+        sys.exit(1)
+
+    # start server
     LOGGER.info("start Outernet server")
 
     init_tun()
